c7a421486102e67429906a4724265ab277604a3f
Panisa Thongthip <panisa.thongthip@gmail.com>
2017-05-22 23:27:35 +0700

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-	//=========== facebook ===========
-	include_once('fb-config.php');
-	include_once('fb-login.php');
-	
-	//=========== google =============
-	include_once 'google-config.php';
-?>

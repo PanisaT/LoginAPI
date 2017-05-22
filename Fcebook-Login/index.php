3d8da8e89384a9793458b7a795c0fb54add02633
Panisa Thongthip <panisa.thongthip@gmail.com>
2017-05-22 23:28:25 +0700

    Create index.php

---
+++ Fcebook-Login/index.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+	//=========== facebook ===========
+	include_once('fb-config.php');
+	include_once('fb-login.php');
+	
+	//=========== google =============
+	include_once 'google-config.php';
+?>
